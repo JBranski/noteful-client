d33b14727a9af4f96927f5a56d1653ba03f81c7a
JBranski <jonathan.branski96@gmail.com>
2021-02-23 11:02:37 -0600

    msg

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-  API_ENDPOINT: ' postgres://gvqrgflxbfzitj:4369b01e21f49629bc9ad3632fdeb7b4a81d2d9f036bf1539aaa5b8aa792f2c8@ec2-52-205-3-3.compute-1.amazonaws.com:5432/dflr39evjdi8fv'
+  API_ENDPOINT: 'https://protected-harbor-96017.herokuapp.com/'
 }
